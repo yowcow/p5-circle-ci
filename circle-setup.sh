8627ddce9cd0e79f000991524de701e859fdf4d6
yowcow <yowcow@gmail.com>
2015-11-12 12:13:41 +0000

    fix

--- circle-setup.sh
+++ circle-setup.sh
@@ -1,16 +1,14 @@
-if test ! -e ~/.anyenv; then
+if [ ! -d ~/.anyenv ]; then
     git clone https://github.com/riywo/anyenv.git ~/.anyenv;
     echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bashrc;
     echo 'eval "$(anyenv init -)"' >> ~/.bashrc;
 fi
 
-if test ! -e ~/.anyenv/envs/plenv; then
-    ~/.anyenv/bin/anyenv install plenv;
+. ~/.bashrc;
+
+if [ ! -d ~/.anyenv/envs/plenv ]; then
+    anyenv install plenv;
 fi
-
-ls -alh;
-
-#. ~/.bashrc;
 
 #if test ! -e ~/.anyenv/envs/plenv/versions/5.20.2; then
 #    plenv install 5.20.2;
